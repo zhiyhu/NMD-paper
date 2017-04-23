d6c5e876b4943bb2d6de586e26c0b6c111736d2c
ZYBunnyHu <zhiyuan.cheryl.hu@gmail.com>
2017-04-23 12:42:50 +0800

    test

--- R/01_PANCAN_data.R
+++ R/01_PANCAN_data.R
@@ -1,6 +1,13 @@
-# read the data
+# read the data----
 
+# set the working directory
+getwd()
+setwd(dir = "../")
+
+# unzip the compressed files
+unzip(zipfile = "data/PANCAN_Xena.zip", exdir = "data/")
 pancan_mut <- read.delim( "data/PANCAN_mutation",header = T,as.is = T)
+pancan_mut_all <- pancan_mut
 pancan_seq <- read.delim( "data/PANCAN_HiSeqV2",header = T,as.is = T)
 pancan_clinic <- read.delim( "data/PANCAN_clinicalMatrix",header = T,as.is = T)
 cancer_type <- read.csv("data/cancer_list.csv",header = T,as.is = T)
@@ -9,25 +16,21 @@ pancan_mut$cancer <- pancan_clinic$X_primary_disease[match(pancan_mut$sample,pan
 pancan_mut$cancer_abbr <- cancer_type$Abbr[match(pancan_mut$cancer,cancer_type$Cancer)]
 cancer_type$Num_all_mut_original <- table(pancan_mut$cancer_abbr)[match(cancer_type$Abbr,names(table(pancan_mut$cancer_abbr)))]
 
-# preprocess pancan_seq
+# preprocess pancan_seq-----
 rownames(pancan_seq) <- pancan_seq$Sample
 pancan_seq <- pancan_seq[,-1]
 colnames(pancan_seq) <- gsub(colnames(pancan_seq),pattern = "[.]",replacement = "-")
 pancan_seq[1:5,1:5]
 dim(pancan_seq)
-# [1] 20530 10459
 
-# add gene entrez id to pancan_mut
+# add gene entrez id to pancan_mut----
 library(org.Hs.eg.db) # map gene name to id by org.Hs.eg.db
 e2s = toTable(org.Hs.egSYMBOL)
 pancan_mut$geneId <- e2s$gene_id[match(pancan_mut$gene,e2s$symbol)]
-rm(e2s)
 
 sum(is.na(pancan_mut$geneId))
-# [1] 92707
 pancan_mut <- pancan_mut[!is.na(pancan_mut$geneId),]
 dim(pancan_mut) #[1] 1895455      13
-View(data.frame(table(pancan_mut$cancer )[order(table(pancan_mut$cancer ),decreasing = T)]))
 
 mut_tb <-table(pancan_mut[,c("sample","gene")])
 sum(mut_tb)
@@ -54,14 +57,9 @@ pancan_mut$duplicated <- F
 pancan_mut$duplicated[which(paste(pancan_mut$sample,pancan_mut$gene) %in% deplicated.mut.sample.gene)] <- T
 rm(deplicated.mut.sample.gene)
 
-table(pancan_mut$duplicated)
-# FALSE    TRUE 
-# 1130496  269726
-
 cancer_type$Num_all_mut_fist_filtered <- table(pancan_mut$cancer_abbr)[match(cancer_type$Abbr,names(table(pancan_mut$cancer_abbr)))]
 
 # full pancan mutations -----
-pancan_mut_all <- read.delim("data/PANCAN_mutation",header = T,as.is = T)
 pancan_mut_all$cancer <- pancan_clinic$X_primary_disease[match(pancan_mut_all$sample,pancan_clinic$sampleID)]
 pancan_mut_all$cancer_abbr <- cancer_type$Abbr[match(pancan_mut_all$cancer,cancer_type$Cancer)]
 
@@ -75,10 +73,12 @@ dim(pancan_mut_all)
 # [1] 1821109      15
 rm(sample.gene)
 
-#~ annotate the multi-mutation in one gene from the same sample-----
+# annotate the multi-mutation in one gene from the same sample-----
 deplicated.mut.sample.gene <- paste(pancan_mut_all$sample,pancan_mut_all$gene)[duplicated(paste(pancan_mut_all$sample,pancan_mut_all$gene))]
 pancan_mut_all$duplicated <- F
 pancan_mut_all$duplicated[which(paste(pancan_mut_all$sample,pancan_mut_all$gene) %in% deplicated.mut.sample.gene)] <- T
 rm(deplicated.mut.sample.gene)
 
 pancan_mut2 <- pancan_mut_all[!(apply(pancan_mut_all[,1:5],1,paste,collapse=" ") %in% apply(pancan_mut[,1:5],1,paste,collapse=" ")),]
+rm(e2s)
+save(file = "01_PANCAN_data_enviroment.RData",compression_level = 9)
